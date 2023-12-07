969eedd118d162cd16a8342eea1e11c7a24c2d85
fullstackDEVH <huagiahuy20052001@gmail.com>
2023-12-07 21:46:11 +0700

    feat : build crud

--- apps/engine/urls.py
+++ apps/engine/urls.py
@@ -5,13 +5,48 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from .views import AllUsersView, UserView, RegisterUserView
+from .views import (
+    AllUsersView,
+    UserView,
+    RegisterUserView,
+    ActivitiesListAPIView,
+    ActivitiesCreateAPIView,
+    ActivitiesDeleteAPIView,
+    ActivitiesDetailAPIView,
+    ActivitiesUpdateAPIView,
+    UserActivitiesAPIView
+)
 
 urlpatterns = [
-    path('', AllUsersView.as_view()),
-    path('user/', UserView.as_view()),
-    path('register/', RegisterUserView.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("users", AllUsersView.as_view()),
+    path("users/detail", UserView.as_view()),
+    path("register/", RegisterUserView.as_view()),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
+
+    ## api for activities 
+    path("activities/", ActivitiesListAPIView.as_view(), name="api-activities-list"),
+    path('activities/<uuid:user_id>/user/', UserActivitiesAPIView.as_view(), name='api-user-activities'),
+    path(
+        "activities/<uuid:pk>/",
+        ActivitiesDetailAPIView.as_view(),
+        name="api-activities-detail",
+    ),
+    path(
+        "activities/create/",
+        ActivitiesCreateAPIView.as_view(),
+        name="api-activities-create",
+    ),
+    path(
+        "activities/<uuid:pk>/update/",
+        ActivitiesUpdateAPIView.as_view(),
+        name="api-activities-update",
+    ),
+    path(
+        "activities/<uuid:pk>/delete/",
+        ActivitiesDeleteAPIView.as_view(),
+        name="api-activities-delete",
+    ),
 ]
